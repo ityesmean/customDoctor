777dcae720756ef06aab0355bc57b06dc7bec48e
송기라 <thdrlfk93@naver.com>
2023-03-20 16:04:30 +0900

    [File] 메인페이지

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import SearchBar from '../components/common/searchBar';
+import Pill from '../assets/Pill.svg';
 
 function Home() {
   return (
     <div>
-      HomePage
-      <SearchBar />
+      <img src={Pill} alt="" />
+
+      {/* <SearchBar /> */}
     </div>
   );
 }
---
+++ front/src/pages/Mainpages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styled from 'styled-components';
+import Pill from '../assets/Pill.svg';
+
+function MainPages() {
+  return (
+    <div>
+      <img src={Pill} alt="" />
+    </div>
+  );
+}
+
+export default MainPages;
