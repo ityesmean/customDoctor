d8e6866a91e5581ba7804a9e33dd384f08f671ed
송기라 <thdrlfk93@naver.com>
2023-03-20 14:39:30 +0900

    [Feat] 검색바 컴포넌트 생성

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import SearchBar from '../components/common/searchBar';
 
 function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <SearchBar />
+    </div>
+  );
 }
 
 export default Home;
