f0265c1aa71120daa1cae2859f9c5d944cffa58d
haninan94 <yuyu1172@gmail.com>
2023-03-21 17:55:00 +0900

    [Design] search bar placholder 배치 수정 및 기타 디자인 수정

--- front/src/pages/HospitalSearch.js
+++ front/src/pages/HospitalSearch.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import BackButton from '../components/common/BackButton';
+
+import Header from '../components/common/Header';
+import SearchBar from '../components/common/SearchBar';
+
+const SLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const STitle1 = styled.div`
+  font-weight: bold;
+  font-size: 7vw;
+  margin-left: 3vw;
+  margin-bottom: 3vh;
+`;
+
+const SLine = styled.div`
+  height: 1vh;
+  background-color: #f1f3f4;
+`;
 
 function HospitalSearch() {
-  return <div>This is HospitalSearch Page.</div>;
+  return (
+    <>
+      <Header />
+      <SLink to="/">
+        <BackButton />
+      </SLink>
+      <STitle1>병원명으로 검색</STitle1>
+      <SearchBar />
+      <SLine> </SLine>
+    </>
+  );
 }
 
 export default HospitalSearch;
