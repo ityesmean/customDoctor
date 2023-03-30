7064e9949ee0ba148739bcf093e293dfe95a7f0c
Anhyogwan <ahg0824@naver.com>
2023-03-30 16:16:43 +0900

    [Feat] OAuth 2.0 로그인 구현
    OAuth2.0 로그인 구현

---
+++ back/src/main/java/com/roller/doc/api/service/auth/OAuth2SuccessHandler.java
@@ -0,0 +1,103 @@
+package com.roller.doc.api.service.auth;
+
+import com.roller.doc.db.entity.Role;
+import com.roller.doc.db.entity.User;
+import com.roller.doc.db.repository.RedisRepository;
+import com.roller.doc.db.repository.UserRepository;
+import com.roller.doc.util.CookieUtil;
+import com.roller.doc.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final TokenService tokenservice;
+    private final UserRepository userRepository;
+    private final RedisUtil redisUtil;
+    private final CookieUtil cookieUtil;
+    private final RedisRepository redisRepository;
+    @Value("${redirect.url}")
+    private String redirectUrl;
+    
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        log.info("Principal에서 꺼낸 OAuth2User = {} ", oAuth2User);
+
+        log.info("DB 등록 확인");
+
+        // 등록되지 않은 회원일경우 회원가입
+        User member = userRepository.findByUserEmail((String) oAuth2User.getAttribute("email"));
+        if (oAuth2User != null && member == null) {
+            User memberData = createMember(oAuth2User);
+            if(memberData == null) {
+                log.info("회원 가입 실패");
+            } else {
+                log.info("유저를 찾을 수 없습니다. 유저 정보를 등록합니다.");
+            }
+        }
+
+        String nickname = userRepository.findByUserEmail((String) oAuth2User.getAttribute("email")).getUserName();
+        log.debug("nickname = {}", nickname);
+        // 닉네임 작성 여부 확인
+        if (nickname == null) {
+            // 닉네임 설정 화면으로
+            log.debug("닉네임 설정 화면으로");
+            getRedirectStrategy().sendRedirect(request, response, UriComponentsBuilder.fromUriString(redirectUrl + "/login/nickname")
+                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
+                    .build().toUriString());
+        } else {
+            // 만약 해당 이메일로 리프레쉬 토큰이 존재한다면 삭제
+            if (redisUtil.getData((String) oAuth2User.getAttribute("email")) != null) {
+                log.info("refresh token exists.Remove refresh token");
+                redisRepository.deleteById((String) oAuth2User.getAttribute("email"));
+            }
+
+            // 리프레쉬 토큰 생성 후 Redis에 등록
+            String refreshtoken = tokenservice.generateToken((String) oAuth2User.getAttribute("email"), "ROLE_MEMBER", nickname, "REFRESH");
+            redisUtil.setDataExpire((String) oAuth2User.getAttribute("email"), refreshtoken, TokenService.refreshPeriod);
+
+            // Response Cookie에 리프레쉬 토큰 적재, access token 생성
+            ResponseCookie cookie = cookieUtil.getCookie(refreshtoken, TokenService.refreshPeriod);
+            String accesstoken = tokenservice.generateToken(oAuth2User.getAttribute("email"), "ROLE_MEMBER", nickname, "ACCESS");
+
+            log.info("accecss_Token = {}", accesstoken);
+            log.info("refresh_Token = {}", refreshtoken);
+            response.setContentType("application/json;charset=UTF-8");
+            // Authorization 헤더필드에 accesstoken 적재 , Set-Cookkie 헤더 필드에 리프레쉬 토큰 cookie 적재
+            response.setHeader("Set-Cookie", cookie.toString());
+
+
+            // 메인으로
+            log.debug("메인으로");
+             getRedirectStrategy().sendRedirect(request, response, UriComponentsBuilder.fromUriString(redirectUrl + "/redirect")
+                    .queryParam("accesstoken", accesstoken)
+                    .build().toUriString());
+        }
+    }
+
+    // 회원 가입시 회원 테이블 생성
+    public User createMember(OAuth2User oAuth2User) {
+        return userRepository.saveAndFlush(User.builder()
+                .userEmail((String) oAuth2User.getAttribute("email"))
+                .userDeleted(false)
+                .userName((String) oAuth2User.getAttribute("nickname"))
+                .userRole(Role.MEMBER)
+                .build());
+    }
+}
