823ccdf6225ff36c6249fd1aaaba3259bac39f37
Jihoon <kjh8673a@naver.com>
2023-03-31 17:54:26 +0900

    [Add] mysql 세팅 파일 수정

--- 05_mysql_window/02_settings.sql
+++ 05_mysql_window/02_settings.sql
@@ -8,13 +8,9 @@ CREATE INDEX idx_drug ON drug ( drug_id );
 CREATE INDEX idx_drug_desc ON drug_desc ( drug_id );
 CREATE INDEX idx_drug_avoid ON drug_avoid ( drug_id );
 
-ALTER TABLE hospital_part ADD FOREIGN KEY ( hospital_id ) references hospital ( hospital_id );
-ALTER TABLE hospital_desc ADD FOREIGN KEY ( hospital_id ) references hospital ( hospital_id );
-ALTER TABLE hospital_time ADD FOREIGN KEY ( hospital_id ) references hospital ( hospital_id ); 
-ALTER TABLE drug_desc ADD FOREIGN KEY ( drug_id ) references drug ( drug_id );
-ALTER TABLE drug_avoid ADD FOREIGN KEY ( drug_id ) references drug ( drug_id );
-
 UPDATE drug SET drug_ingre = 'null' WHERE drug_ingre IS NULL;
 UPDATE drug_desc SET drug_desc_safety = 'null' WHERE drug_desc_safety IS NULL;
 UPDATE drug_desc SET drug_desc_effect = 'null' WHERE drug_desc_effect IS NULL;
 UPDATE drug_desc SET drug_desc_use = 'null' WHERE drug_desc_use IS NULL;
+
+ALTER TABLE hospital ADD FULLTEXT INDEX idx_hospital_name(hospital_name) WITH PARSER NGRAM;
