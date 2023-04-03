96848ec8d93dfe0d9f13fdc0486b0e160b6db435
송기라 <thdrlfk93@naver.com>
2023-04-03 09:42:54 +0900

    [Fix] 병원 상세페이지 안뜨는 오류 수정 timetable 이 null 상태일때를 가정하지 않아서 오류뜨는 거였음 삼항 연산자로 처리

--- front/src/components/kakao/KakaoMap.js
+++ front/src/components/kakao/KakaoMap.js
@@ -24,11 +24,11 @@ function KakaoMap({ x, y }) {
     hospitalSearchResultState,
   );
 
-  console.log(x, y)
+  // console.log(x, y)
 
-  console.log(hospitalSearchResult)
+  // console.log(hospitalSearchResult)
 
-  console.log(hospitalSearchResult);
+  // console.log(hospitalSearchResult);
   useEffect(() => {
     var mapContainer = document.getElementById('map'); // 지도를 표시할 div
     var mapOption = {
@@ -36,39 +36,41 @@ function KakaoMap({ x, y }) {
       level: 3, // 지도의 확대 레벨
     };
 
-    console.log(mapContainer);
-    console.log(mapOption);
+    // console.log(mapContainer);
+    // console.log(mapOption);
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
     // 마커 이미지 주소
-    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+    var imageSrc =
+      'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
-    console.log(hospitalSearchResult.lenght)
+    // console.log(hospitalSearchResult.lenght);
     // 마커들을 생성합니다.
     for (var i = 0; i < hospitalSearchResult.length; i++) {
       var imageSize = new kakao.maps.Size(24, 35);
 
       // 마커 이미지를 생성합니다.
-      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
       // test
-      console.log(hospitalSearchResult[i].hospitalX)
-      console.log(hospitalSearchResult[i].hospitalY)
-      console.log(hospitalSearchResult[i].hospitalName)
+      // console.log(hospitalSearchResult[i].hospitalX);
+      // console.log(hospitalSearchResult[i].hospitalY);
+      // console.log(hospitalSearchResult[i].hospitalName);
 
       // 마커를 생성합니다.
       var marker = new kakao.maps.Marker({
         map, // 마커를 표시할 지도
-        position: new kakao.maps.LatLng(hospitalSearchResult[i].hospitalY, hospitalSearchResult[i].hospitalX), // 마커를 표시할 위치
+        position: new kakao.maps.LatLng(
+          hospitalSearchResult[i].hospitalY,
+          hospitalSearchResult[i].hospitalX,
+        ), // 마커를 표시할 위치
         // title: hospitalSearchResult[i].hospitalName, // 마커의 타이틀
-        image: markerImage // 마커 이미지
-      })
-      console.log('그린다')
+        image: markerImage, // 마커 이미지
+      });
       marker.setMap(map);
     }
-
   }, []);
 
   const tempOnClick = () => {
