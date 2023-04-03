dc9f9df4bd35e96521c59742c740becbbb70620e
haninan94 <yuyu1172@gmail.com>
2023-04-03 17:11:49 +0900

    [Test] kakao api 정보 사용 폐기

--- front/src/components/kakao/KakaoMap.js
+++ front/src/components/kakao/KakaoMap.js
@@ -16,10 +16,7 @@
 import React, { useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import PropTypes from 'prop-types';
-import { useNavigate, Link } from 'react-router-dom';
 import styled from 'styled-components';
-
-import BackButton from '../common/BackButton';
 
 import { hospitalSearchResultState, myPositionState } from '../../atoms';
 
@@ -38,115 +35,54 @@ const { kakao } = window;
 function KakaoMap({ lat, lng }) {
   const myPosition = useRecoilValue(myPositionState);
 
-  const navigate = useNavigate();
   const [hospitalSearchResult, setHospitalSearchResult] = useRecoilState(
     hospitalSearchResultState,
   );
 
   useEffect(() => {
-    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-
     var mapContainer = document.getElementById('map'); // 지도를 표시할 div
     var mapOption = {
-      center: new kakao.maps.LatLng(myPosition[0], myPosition[1]), // 지도의 중심좌표
-      level: 5, // 지도의 확대 레벨
+      center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
     };
 
-    console.log(myPosition[0], myPosition[1]);
+    // console.log(mapContainer);
+    // console.log(mapOption);
 
-    // 지도를 생성합니다
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
-    // 장소 검색 객체를 생성합니다
-    var ps = new kakao.maps.services.Places(map);
+    // 마커 이미지 주소
+    var imageSrc =
+      'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
-    // 지도에 마커를 표시하는 함수입니다
-    function displayMarker(place) {
-      // 마커를 생성하고 지도에 표시합니다
+    // console.log(hospitalSearchResult.lenght);
+    // 마커들을 생성합니다.
+    for (var i = 0; i < hospitalSearchResult.length; i++) {
+      var imageSize = new kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다.
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다.
       var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x),
+        map, // 마커를 표시할 지도
+        position: new kakao.maps.LatLng(
+          hospitalSearchResult[i].hospitalY,
+          hospitalSearchResult[i].hospitalX,
+        ), // 마커를 표시할 위치
+        // title: hospitalSearchResult[i].hospitalName, // 마커의 타이틀
+        image: markerImage, // 마커 이미지
       });
-
-      // 마커에 클릭이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, 'click', function () {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent(
-          '<div style="padding:5px;font-size:12px;">' +
-            place.place_name +
-            '</div>',
-        );
-        infowindow.open(map, marker);
-        console.log(place.place_name);
-      });
+      marker.setMap(map);
     }
+  }, [lat, lng]);
 
-    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-    function placesSearchCB(data, status, pagination) {
-      if (status === kakao.maps.services.Status.OK) {
-        for (var i = 0; i < data.length; i++) {
-          displayMarker(data[i]);
-        }
-      }
-    }
-
-    // 카테고리로 은행을 검색합니다
-    ps.categorySearch('HP8', placesSearchCB, { useMapBounds: true });
-  });
-
-  // 1안 코드 -----------------------------------------------------------------
-  // useEffect(() => {
-  //   var mapContainer = document.getElementById('map'); // 지도를 표시할 div
-  //   var mapOption = {
-  //     center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
-  //     level: 3, // 지도의 확대 레벨
-  //   };
-
-  //   // console.log(mapContainer);
-  //   // console.log(mapOption);
-
-  //   // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-  //   var map = new kakao.maps.Map(mapContainer, mapOption);
-
-  //   // 마커 이미지 주소
-  //   var imageSrc =
-  //     'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
-
-  //   // console.log(hospitalSearchResult.lenght);
-  //   // 마커들을 생성합니다.
-  //   for (var i = 0; i < hospitalSearchResult.length; i++) {
-  //     var imageSize = new kakao.maps.Size(24, 35);
-
-  //     // 마커 이미지를 생성합니다.
-  //     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-  //     // 마커를 생성합니다.
-  //     var marker = new kakao.maps.Marker({
-  //       map, // 마커를 표시할 지도
-  //       position: new kakao.maps.LatLng(
-  //         hospitalSearchResult[i].hospitalY,
-  //         hospitalSearchResult[i].hospitalX,
-  //       ), // 마커를 표시할 위치
-  //       // title: hospitalSearchResult[i].hospitalName, // 마커의 타이틀
-  //       image: markerImage, // 마커 이미지
-  //     });
-  //     marker.setMap(map);
-  //   }
-  // }, [lat, lng]);
-
-  // const tempOnClick = () => {
-  //   navigate(-1);
-  // };
   // 1안 코드 -----------------------------------------------------------------
 
   return (
     <>
-      {/* <Link to="/hospital/search/result"> */}
-      {/* <div onClick={tempOnClick}> */}
-      <BackButton />
-      {/* </div> */}
       <SReSearchButton>현 위치에서 검색</SReSearchButton>
-      {/* </Link> */}
       <div id="map" style={{ width: '100vw', height: '95vh' }}></div>
     </>
   );
--- front/src/pages/hospital/HospitalDetail.js
+++ front/src/pages/hospital/HospitalDetail.js
@@ -1,19 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import axios from 'axios';
 
 import { API_URL_HOSPITAL } from '../../api/api';
 import { hospitalDescState, hospitalBasicState } from '../../atoms';
+import BackButton from '../../components/common/BackButton';
 import KakaoMap from '../../components/kakao/KakaoMap';
 import HositalTotal from '../../components/hospital/HositalTotal';
+
+const SBackButton = styled.button`
+  border: none;
+  background: none;
+`;
 
 const SContainer = styled.div``;
 
 function HospitalDetail() {
   const information = useLocation();
+  const navigate = useNavigate();
   const { hospitalId } = useParams();
   const lat = information.state.information.hospitalY;
   const lng = information.state.information.hospitalX;
@@ -30,14 +37,23 @@ function HospitalDetail() {
       .catch(err => console.log(err));
   };
 
+  const onClickBackButtonHandler = () => {
+    navigate(-1);
+  };
+
   useEffect(() => {
     getInformation();
   }, []);
 
   return (
-    <SContainer>
-      <KakaoMap lat={lat} lng={lng} />
-    </SContainer>
+    <>
+      <SBackButton type="button" onClick={onClickBackButtonHandler}>
+        <BackButton />
+      </SBackButton>
+      <SContainer>
+        <KakaoMap lat={lat} lng={lng} />
+      </SContainer>
+    </>
   );
 }
 
