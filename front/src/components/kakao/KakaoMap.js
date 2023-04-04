d5b343b36d6de66fee369de1b337dd0a370d5552
haninan94 <yuyu1172@gmail.com>
2023-04-04 15:52:56 +0900

    [Feat] kakaomap overlay 구현

--- front/src/components/kakao/KakaoMap.js
+++ front/src/components/kakao/KakaoMap.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-var */
 /* eslint-disable no-undef */
+/* eslint-disable no-shadow */
 /* eslint-disable func-names */
 /* eslint-disable no-plusplus */
 /* eslint-disable vars-on-top */
@@ -9,13 +10,14 @@
 /* eslint-disable block-scoped-var */
 /* eslint-disable object-shorthand */
 /* eslint-disable prefer-destructuring */
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-inner-declarations */
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* global kakao */
-import React, { useEffect } from 'react';
+import React, { createElement, useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -23,6 +25,8 @@ import styled from 'styled-components';
 import { hospitalSearchResultState, myPositionState } from '../../atoms';
 
 import './Overlay.css';
+
+import GreenHospital from '../../assets/mypage/GreenHospital.png';
 
 const SReSearchButton = styled.button`
   position: absolute;
@@ -46,19 +50,17 @@ function KakaoMap({ lat, lng }) {
   console.log(hospitalSearchResult);
 
   // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
-  function closeOverlay() {
-    overlay.setMap(null);
-  }
+  // function closeOverlay() {
+  //   overlay.setMap(null);
+  // }
 
   useEffect(() => {
     var mapContainer = document.getElementById('map'); // 지도를 표시할 div
     var mapOption = {
       center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
+      draggable: true,
       level: 3, // 지도의 확대 레벨
     };
-
-    // console.log(mapContainer);
-    // console.log(mapOption);
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
@@ -67,85 +69,101 @@ function KakaoMap({ lat, lng }) {
     var imageSrc =
       'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
-    // console.log(hospitalSearchResult.lenght);
+    var imageSize = new kakao.maps.Size(24, 35);
+
+    // 마커 이미지를 생성합니다.
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
     // 마커들을 생성합니다.
-    for (var i = 0; i < hospitalSearchResult.length; i++) {
-      var imageSize = new kakao.maps.Size(24, 35);
 
-      // 마커 이미지를 생성합니다.
-      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+    // 마커와 커스텀 오버레이 순회
+    for (let i = 0; i < hospitalSearchResult.length; i++) {
+      var data = hospitalSearchResult;
+      displayMarker(data[i]);
+    }
 
+    function displayMarker(data) {
       // 마커를 생성합니다.
+
+      console.log(data);
       var marker = new kakao.maps.Marker({
         map, // 마커를 표시할 지도
-        position: new kakao.maps.LatLng(
-          hospitalSearchResult[i].hospitalY,
-          hospitalSearchResult[i].hospitalX,
-        ), // 마커를 표시할 위치
+        position: new kakao.maps.LatLng(data.hospitalY, data.hospitalX), // 마커를 표시할 위치
 
-        // title: hospitalSearchResult[i].hospitalName, // 마커의 타이틀
         image: markerImage, // 마커 이미지
       });
 
-      // var infowindow = new kakao.maps.InfoWindow({
-      //   // content:
-      //   //   '<div>' +
-      //   //   hospitalSearchResult[i].hospitalName +
-      //   //   '</div>' +
-      //   //   '<div>' +
-      //   //   hospitalSearchResult[i].hospitalTel +
-      //   //   '</div>',
-      // });
-
       // 커스텀 오버레이를 표시할 좌표
-      var position = new kakao.maps.LatLng(
-        hospitalSearchResult[i].hospitalY,
-        hospitalSearchResult[i].hospitalX,
-      );
-
-      // 커스텀 오버레이에 들어갈 content
-      // var content =
-      //   '<div>' +
-      //   '  <div>' +
-      //   hospitalSearchResult[i].hospitalName +
-      //   '  </div>' +
-      //   '  <div onClick="closeOverlay()" title="닫아보자">' +
-      //   '    닫기' +
-      //   '  </div>' +
-      //   '</div>';
-
-      var content =
-        '<div class="wrap">' +
-        '    <div class="info">' +
-        '        <div class="title">' +
-        hospitalSearchResult[i].hospitalName +
-        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
-        '        </div>' +
-        '        <div class="body">' +
-        '            <div class="img">' +
-        '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
-        '           </div>' +
-        '            <div class="desc">' +
-        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-        hospitalSearchResult[i].hospitalTel +
-        '            </div>' +
-        '        </div>' +
-        '    </div>' +
-        '</div>';
+      var position = new kakao.maps.LatLng(data.hospitalY, data.hospitalX);
 
       var overlay = new kakao.maps.CustomOverlay({
-        content,
-        map,
+        // 맵 주석 해제하면 처음에 overlay 닫혀있음
+        // map,
         position,
+
+        // 오버레이 클릭하면 지도 이벤트 막기
+        clickable: true,
       });
 
-      marker.setMap(map);
+      // 오버레이 html 생성
+      var content = document.createElement('div');
+      content.className = 'wrap';
 
-      // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+      var info = document.createElement('div');
+      info.className = 'info';
+
+      var title = document.createElement('div');
+      title.className = 'title';
+      // 병원 이름의 길이가 10자 이상이면 10번째 까지 slicing
+      if (data.hospitalName.length >= 10) {
+        var filteredHospitalName = data.hospitalName.substr(0, 10) + '...';
+      } else {
+        filteredHospitalName = data.hospitalName;
+      }
+      title.innerHTML = filteredHospitalName;
+      data.hospitalName.substr(0, 10);
+
+      var close = document.createElement('div');
+      close.className = 'close';
+      close.setAttribute('title', '닫기');
+
+      var body = document.createElement('div');
+      body.className = 'body';
+
+      var imgDiv = document.createElement('div');
+      imgDiv.className = 'img';
+
+      var desc = document.createElement('div');
+      desc.className = 'desc';
+
+      var img = document.createElement('img');
+      img.className = 'hospitalImage';
+      img.setAttribute('src', GreenHospital);
+
+      var address = document.createElement('div');
+      address.className = 'ellipsis';
+      address.innerHTML = '임시 주소';
+
+      var tel = document.createElement('tel');
+      tel.innerHTML = data.hospitalTel;
+
+      content.append(info);
+      info.append(title, body);
+      title.append(close);
+      body.append(imgDiv, desc);
+      imgDiv.append(img);
+      desc.append(address, tel);
+
+      overlay.setContent(content);
+
+      close.onclick = function () {
+        console.log('악');
+        overlay.setMap(null);
+      };
       kakao.maps.event.addListener(marker, 'click', function () {
         overlay.setMap(map);
       });
+
+      // marker.setMap(map);
     }
   }, []);
 
