486e922f5e8b753c7120c0ead63e068c04f89239
송기라 <thdrlfk93@naver.com>
2023-04-05 13:44:02 +0900

    [Refactor] getFavoriteList 이동 getFavoriteList를 HospitalList 컴포넌트에서 HospitalFavoriteList 컴포넌트로 이동

--- front/src/components/hospital/HospitalFavoriteList.js
+++ front/src/components/hospital/HospitalFavoriteList.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useState, useEffect } from 'react';
+// import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-import { hospitalFavoriteState } from '../../atoms';
+// import { hospitalFavoriteState } from '../../atoms';
 import HospitalCard from './HospitalCard';
 
 const SContainer = styled.div``;
@@ -15,9 +16,57 @@ const SLink = styled(Link)`
 `;
 
 function hospitalFavoriteList() {
-  const favoriteList = useRecoilValue(hospitalFavoriteState);
-  console.log(favoriteList, 'favoriteList');
+  // const favoriteList = useRecoilValue(hospitalFavoriteState);
+  // console.log(favoriteList, 'favoriteList');
+  const [favoriteList, setFavoriteList] = useState([]);
 
+  const currentTime = new Date();
+  const currentDay = currentTime.getDay();
+  const currentHours = currentTime.getHours();
+  const currentMinutes = currentTime.getMinutes();
+
+  const token = localStorage.getItem('accessToken');
+
+  const getFavoriteList = async () => {
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/hospital/marklist`,
+        // { withCredentials: true },
+        { hour: currentHours, min: currentMinutes, day: currentDay },
+        { headers: { Authorization: `${token}` } },
+      );
+      // if (res.data.status_code === 204) {
+      //   console.log(res.data.data, '204');
+      // } else {
+      setFavoriteList(res.data.data);
+      console.log(res.data.data, '200');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    // const getFavoriteList = async () => {
+    //   try {
+    //     const res = await axios.post(
+    //       `${process.env.REACT_APP_API_URL}/user/hospital/marklist`,
+    //       // { withCredentials: true },
+    //       { hour: currentHours, min: currentMinutes, day: currentDay },
+    //       { headers: { Authorization: `${token}` } },
+    //     );
+    //     // if (res.data.status_code === 204) {
+    //     //   console.log(res.data.data, '204');
+    //     // } else {
+    //     setFavoriteList(res.data.data);
+    //     console.log(res.data.data, '200');
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+    if (token) {
+      getFavoriteList();
+    }
+  }, []);
   return (
     <SContainer>
       {favoriteList.map((faovrite, index) => (
